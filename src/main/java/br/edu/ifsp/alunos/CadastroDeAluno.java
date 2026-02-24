9f03eb2dfe3fc0e07128627292333a27262f3c5d
Letícia <leticia.yabuuti@aluno.ifsp.edu.br>
2026-02-23 21:00:06 -0300

    commit inicial

---
+++ src/main/java/br/edu/ifsp/alunos/CadastroDeAluno.java
@@ -0,0 +1,23 @@
+package br.edu.ifsp.alunos;
+
+import br.edu.ifsp.alunos.model.Aluno;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class CadastroDeAluno {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno();
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
+
+        EntityManager em = factory.createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.persist(aluno1);
+
+        em.getTransaction().commit();
+
+        em.close();
+    }
+}
---
+++ src/main/java/br/edu/ifsp/alunos/model/Aluno.java
@@ -0,0 +1,11 @@
+package br.edu.ifsp.alunos.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "alunos")
+public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
